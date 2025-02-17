80d5acf392714e533b273c2bb1f1ccfa1b295228
ნოდო ცინაძე <144336325+nodocinadze@users.noreply.github.com>
2025-02-17 19:51:14 +0400

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,12 @@
+# def palindrome(level):
+
+#     if level == level [::-1]:
+#         print("is palindrome")
+#     else:
+#         print("not palindrome")
+
+
+def find_min_max(lst):
+    return min(lst), max(lst)
+
+    
